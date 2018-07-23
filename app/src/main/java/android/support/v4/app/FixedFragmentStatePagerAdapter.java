fbdbbaa5a4644ede383e6dbf109f1e3c096cc9cb
Nirosh Gunaratne <niroshpg@gmail.com>
2018-07-24 01:01:25 +1000

    upgrade to latest android fix usgs url

---
+++ app/src/main/java/android/support/v4/app/FixedFragmentStatePagerAdapter.java
@@ -0,0 +1,10 @@
+package android.support.v4.app;
+
+import FragmentStatePagerAdapter;
+
+/**
+ * Created by nirosh on 23/07/2018.
+ */
+
+public class FixedFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+}
